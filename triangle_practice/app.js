93fd80432e3256d23606c29684f2c1ba6a288699
skulleci <77619047+skulleci@users.noreply.github.com>
2021-03-17 23:20:10 +0200

    Update app.js

--- triangle_practice/app.js
+++ triangle_practice/app.js
@@ -21,9 +21,9 @@ const alanHesap = () => {
 };
 
 const cevreHesap = () => {
-    const kenar1 = Math.sqrt((Math.pow(2,(x2-x1))+(Math.pow(2,(y2-y1)))));
-    const kenar2 = Math.sqrt((Math.pow(2,(x3-x1))+(Math.pow(2,(y3-y1)))));
-    const kenar3 = Math.sqrt((Math.pow(2,(x3-x2))+(Math.pow(2,(y3-y2)))));
+    const kenar1 = Math.sqrt((Math.pow((x2-x1),2)+(Math.pow((y2-y1),2))));
+    const kenar2 = Math.sqrt((Math.pow((x3-x1),2)+(Math.pow((y3-y1),2))));
+    const kenar3 = Math.sqrt((Math.pow((x3-x2),2)+(Math.pow((y3-y2),2))));
     const cevre = kenar1+kenar2+kenar3;
     return cevre;
 };
