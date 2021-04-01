ac417dace14ad0fc2ad3cd20d6c6788bf786ca66
skulleci <77619047+skulleci@users.noreply.github.com>
2021-04-01 14:55:01 +0300

    new

--- API_practice/app.js
+++ API_practice/app.js
@@ -1,4 +1,8 @@
 let selectButton = document.querySelector("#selectButton");
+let todosList = document.querySelector(".list-group");
+let li = document.createElement("li");
+let searchBar = document.getElementById("searchBar");
+let searchButton = document.querySelector("#searchButton");
 
 const users = axios.get('https://jsonplaceholder.typicode.com/users/');
 users.then(response => {
@@ -15,50 +19,59 @@ users.then(response => {
 });
 
 selectButton.addEventListener("change", (event) => {
+    clearList();
+    setList(event);
     
-    const todos = axios.get(`https://jsonplaceholder.typicode.com/todos?userId=${event.target.value}`);
+});
+
+function setList() {
+    let todos = axios.get(`https://jsonplaceholder.typicode.com/todos?userId=${event.target.value}`);
     todos.then(response => {
         return response.data;
     }).then(data => {
-        data.forEach(data => {
-            const li = document.createElement("li");
-            li.className = "liler"
-            li.id = data.id;
-            li.innerHTML = data.title + " " + "Completed : " + data.completed;
+        data.forEach(todo => {
+            li = document.createElement("li");
+            li.setAttribute("id", todo.id);
+            li.innerHTML = todo.title + " " + "Completed : " + todo.completed;
             document.querySelector(".list-group").appendChild(li);
-            
-            
         });
-        
     });
-});
+}
 
-/*
-        for (let index = 0; index < data.length; index++) {
+function clearList() {
+    while (todosList.firstChild) {
+        todosList.removeChild(todosList.firstChild);
+    }
+}
 
-            const li = document.createElement("li");
-            document.querySelector(".dropdown-menu").appendChild(li);
+// function getRelevancy(value, searchTerm) {
+//     if(value === searchTerm) {
+//         return 2;
+//     } else if (value.startsWith(term)){
+//         return 1;
+//     } else if(value.includes(searchTerm)) {
+//         return 0;
+//     } else {
+//         return -1;
+//     }
+// }
 
-            const a = document.createElement("a");
-            li.appendChild(a);
-            a.className = "dropdown-item";
-            a.innerHTML = data[index].id + " - " + data[index].name ;
-            a.id = data[index].id;
-            a.addEventListener("click",  (event) => {
-                const todos = axios.get(`https://jsonplaceholder.typicode.com/todos?userId=`${});
-                todos.then(response => {
-                    return response.data;
-                }).then(data => {
-                    for (let index = 0; index < data.length; index++) {
-                        console.log(data[index]);
-                        
-                    }
-                    return data;
-                });
-            })   
-            console.log(data[index]);
+// function search() {
+//     searchBar.addEventListener('input', (event) => {
+//         let value = event.target.value;
+//         if (value && value.trim().lenght > 0) {
+//             value = value.trim().toLowerCase();
+//             setList(data.filter(todo => {
+//                 return todo.completed.includes(value);
+//             }).sort((a,b) => {
+//                 return getRelevancy(b.completed, value) - getRelevancy(a.completed, value);
+//             }));
+//         } else {
+//             clearList();
+//         }
+//     });
+// }
 
-        }   
-     
-    });
-*/
+// searchButton.addEventListener("click", (event) => {
+//     search(event);
+// })
